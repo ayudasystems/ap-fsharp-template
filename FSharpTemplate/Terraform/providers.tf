0f61fb17a164d20056f74fbc6b9b311809d6948e
SergioHerrero <sergio.herrero@broadsign.com>
2022-06-08 17:13:15 +0200

    Remove credentials from repository

--- FSharpTemplate/Terraform/providers.tf
+++ FSharpTemplate/Terraform/providers.tf
@@ -15,10 +15,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-#   Service Principal
-#   Using ap-terraform (not live - temp)
-#  subscription_id   = "b9f2d614-47a6-4974-9a3a-9d260db71e07" // "<azure_subscription_id>"
-#  tenant_id         = "baf00b3a-3dce-4b83-a82c-e4ffaee51f84" // "<azure_subscription_tenant_id>"
-#  client_id         = "771e6187-899a-4a94-b9ca-7ec66fa153e1" // "<service_principal_appid>"
-#  client_secret     = "aRr8Q~QVMrCgvobG60hULPuNgtOYfXiDc4DdfcDu" // "<service_principal_password>"
 }
