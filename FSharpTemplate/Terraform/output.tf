adb8caa71c317567b1a1597605c67772c43121bd
SergioHerrero <sergio.herrero@broadsign.com>
2022-06-29 16:46:04 +0200

    Clean CircleCI config file, comment CircleCI hidden output values, terraform apply command

--- FSharpTemplate/Terraform/output.tf
+++ FSharpTemplate/Terraform/output.tf
@@ -6,15 +6,15 @@
 #   echo "$(terraform output resource_group_name)"
 
 # Example: Resource Group Name
-output "resource_group_name" {
-  value       = azurerm_resource_group.rg.name
-  description = "Resource Group name"
-}
-
-output "service_plan_name" {
-  value       = azurerm_service_plan.sp.name
-  description = "Service Plan name"
-}
+#output "resource_group_name" {
+#  value       = azurerm_resource_group.rg.name
+#  description = "Resource Group name"
+#}
+#
+#output "service_plan_name" {
+#  value       = azurerm_service_plan.sp.name
+#  description = "Service Plan name"
+#}
 
 output "app_service_name" {
   value       = azurerm_windows_web_app.as.name
