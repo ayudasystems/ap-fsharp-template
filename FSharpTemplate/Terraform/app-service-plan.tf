b746408276150501c6c8fd9f1ae19d6b991a315f
jimmyc <jimmy.cyr@broadsign.com>
2022-06-30 15:22:31 -0400

    use linux app service plan, use linux app service

--- FSharpTemplate/Terraform/app-service-plan.tf
+++ FSharpTemplate/Terraform/app-service-plan.tf
@@ -5,6 +5,6 @@ resource "azurerm_service_plan" "sp" {
   name                = var.service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"
+  os_type             = "Linux"
   sku_name            = var.service_plan_sku_name
 }
--- FSharpTemplate/Terraform/app-service.tf
+++ FSharpTemplate/Terraform/app-service.tf
@@ -18,9 +18,8 @@ resource "azurerm_windows_web_app" "as" {
   site_config {
 
     application_stack {
-      docker_container_registry = var.docker_registry_server_url
-      docker_container_name     = var.docker_container_name
-      docker_container_tag      = var.docker_container_tag
+      docker_image     = "${var.docker_registry_server_url}/${var.docker_container_name}:${var.docker_container_tag}"
+      docker_image_tag = var.docker_container_tag
     }
 
     #    windows_fx_version = "DOCKER|${azurerm_container_registry.cr.name}/${var.docker_container_tag}"
