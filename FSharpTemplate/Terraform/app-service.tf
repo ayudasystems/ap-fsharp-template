09fe43de04681221bd60b40eb618c1beb66af816
SergioHerrero <sergio.herrero@broadsign.com>
2022-06-30 12:34:55 +0200

    Fix application_stack test issues.

--- FSharpTemplate/Terraform/app-service.tf
+++ FSharpTemplate/Terraform/app-service.tf
@@ -16,7 +16,8 @@ resource "azurerm_windows_web_app" "as" {
     application_stack {
       docker_container_name     = var.docker_container_name
       docker_container_registry = var.docker_container_registry
-      docker_container_tag      = var.docker_container_tag
+      docker_container_tag      = "latest"
+#      docker_container_tag      = var.docker_container_tag
     }
 
   }
