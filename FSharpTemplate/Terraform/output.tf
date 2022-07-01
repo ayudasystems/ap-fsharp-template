90d4b7e390e9a004f0ccd5c5319c7d6be8e4b8af
SergioHerrero <sergio.herrero@broadsign.com>
2022-07-01 16:24:02 +0200

    Trying to deploy docker image for Windows

--- FSharpTemplate/Terraform/output.tf
+++ FSharpTemplate/Terraform/output.tf
@@ -17,6 +17,6 @@
 #}
 
 output "app_service_name" {
-  value       = azurerm_linux_web_app.as.name
+  value       = azurerm_windows_web_app.as.name
   description = "Deployed Web App Service name"
 }
