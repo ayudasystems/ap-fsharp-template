2bfa58e6a08afdfdc7ec470c349b744489e49ba9
SergioHerrero <sergio.herrero@broadsign.com>
2022-07-04 18:17:43 +0200

    Introduce Application Insights

---
+++ FSharpTemplate/Terraform/application-insights.tf
@@ -0,0 +1,9 @@
+# terraform/application-insights.tf
+
+# Create Application Insights related to Service Resource
+#resource "azurerm_application_insights" "ai" {
+#  name                = "${var.service_name}${var.environment_suffix}-appinsights"
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
+#  application_type    = "web"
+#}
