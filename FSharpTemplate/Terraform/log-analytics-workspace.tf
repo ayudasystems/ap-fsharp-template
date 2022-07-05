6b9fe031631eab62b8788b9047e686b24b4a0882
SergioHerrero <sergio.herrero@broadsign.com>
2022-07-05 09:05:20 +0200

    Application Insights improvement

--- FSharpTemplate/Terraform/log-analytics-workspace.tf
+++ FSharpTemplate/Terraform/log-analytics-workspace.tf
@@ -5,4 +5,5 @@ resource "azurerm_log_analytics_workspace" "law" {
   name                = "${var.service_name}${var.environment_suffix}-law"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  retention_in_days   = 30
 }
