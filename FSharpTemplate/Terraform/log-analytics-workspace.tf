e927df5dece204647a2655deeaa9076bf20e6fe2
jimmyc <jimmy.cyr@broadsign.com>
2022-07-05 07:30:31 -0400

    add storage account to store app service logs

--- FSharpTemplate/Terraform/log-analytics-workspace.tf
+++ FSharpTemplate/Terraform/log-analytics-workspace.tf
@@ -7,3 +7,11 @@ resource "azurerm_log_analytics_workspace" "law" {
   location            = azurerm_resource_group.rg.location
   retention_in_days   = 30
 }
+
+# Create Log Analytics Workspace linked storage account
+resource "azurerm_log_analytics_linked_storage_account" "sa" {
+  data_source_type      = "customlogs"
+  resource_group_name   = azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.law.id
+  storage_account_ids   = [azurerm_storage_account.sa.id]
+}
---
+++ FSharpTemplate/Terraform/storage-account.tf
@@ -0,0 +1,37 @@
+# terraform/application-insights.tf
+
+# Create Storage Account for the project
+resource "azurerm_storage_account" "sa" {
+  name                     = "${var.service_name}${var.environment_suffix}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
+}
+
+# Create Log Storage Blob Container
+resource "azurerm_storage_container" "lsc" {
+  name                 = "${var.service_name}${var.environment_suffix}-logs"
+  storage_account_name = azurerm_storage_account.sa.name
+}
+
+# Create Log Storage Blob Container SAS
+data "azurerm_storage_account_blob_container_sas" "lscsas" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  container_name    = azurerm_storage_container.lsc.name
+  https_only        = true
+  start             = "2022-07-05"
+  expiry            = "2023-07-05"
+  permissions {
+    read   = true
+    add    = true
+    create = false
+    write  = true
+    delete = true
+    list   = true
+  }
+  content_language = "en-US"
+  content_type     = "application/json"
+}
