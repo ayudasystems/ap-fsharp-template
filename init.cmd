5243ba273f723e109bbfef8273724c9f8686171b
Meheson Bacud <meheson.bacud@broadsign.com>
2022-07-07 13:17:39 +0200

    PLATFORM-247: Update input messages

--- init.cmd
+++ init.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-if [%1]==[] (set /p SOLUTION_NAME="Enter solution name: ") else (set SOLUTION_NAME=%1)
-if [%2]==[] (set /p PROJECT_NAME="Enter project name: ") else (set PROJECT_NAME=%2)
-if [%3]==[] (set /p OUTPUT_PATH="Enter output path: ") else (set OUTPUT_PATH=%3)
+if [%1]==[] (set /p SOLUTION_NAME="Enter Solution name: ") else (set SOLUTION_NAME=%1)
+if [%2]==[] (set /p PROJECT_NAME="Enter Project name: ") else (set PROJECT_NAME=%2)
+if [%3]==[] (set /p OUTPUT_PATH="Enter Solution directory: ") else (set OUTPUT_PATH=%3)
 set CURRENT_DIR=%cd%
 
 dotnet new --install .
