28b99dc3502c82795cec732fa958efa27cf387bc
jimmyc <jimmy.cyr@broadsign.com>
2022-07-28 17:40:16 -0400

    PLATFORM-261: add role assignment to user-assigned managed identity

--- Terraform/role-assignment.tf
+++ Terraform/role-assignment.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "acrpull" {
-  scope                = azurerm_container_registry.cr
+  scope                = azurerm_container_registry.cr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_user_assigned_identity.uai.principal_id
 }
