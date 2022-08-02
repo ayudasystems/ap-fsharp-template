a01b485d1762afbefc9d4729d480d43de3502ca4
Jimmy Cyr <aphiroth@gmail.com>
2022-08-02 09:07:02 -0400

    PLATFORM-261: use unique resource group (#7)
    * PLATFORM-261: use unique service principal for service
    
    * PLATFORM-261: test deployment
    
    * PLATFORM-261: test deployment
    
    * PLATFORM-261: make sure to reuse the app plan
    
    * PLATFORM-261: add role assignment to user-assigned managed identity
    
    * PLATFORM-261: add role assignment to user-assigned managed identity
    
    * PLATFORM-261: remove deployment test
    
    * PLATFORM-261: make changes for all environments
    
    * PLATFORM-261: deployment test
    
    Co-authored-by: jimmyc <jimmy.cyr@broadsign.com>

--- Terraform/resource-group.tf
+++ Terraform/resource-group.tf
@@ -2,6 +2,11 @@
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
+  name     = "${var.service_name}${var.environment_suffix_lowercase}"
+  location = var.resource_group_location
+}
+
+resource "azurerm_resource_group" "app_plan_rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -12,6 +12,11 @@ variable "environment" {
   description = "Environment (Ayuda Dev / Ayuda Preview / Ayuda Cloud)"
 }
 
+variable "resource_group_name" {
+  default     = "ayudalabs-na-01"
+  description = "Name of the app plan resource group."
+}
+
 variable "service_name" {
   default     = "FSharpTemplate"
   type        = string
@@ -24,9 +29,10 @@ variable "environment_suffix" {
   description = "Environment Suffix. E.g.: Ayuda Dev -> -NA-CI, Ayuda Preview -> -EU-UAT"
 }
 
-variable "resource_group_name" {
-  default     = "ayudalabs-na-01"
-  description = "Name of the resource group."
+variable "environment_suffix_lowercase" {
+  default     = "-na-ci"
+  type        = string
+  description = "Environment Suffix. E.g.: Ayuda Dev -> -na-ci, Ayuda Preview -> -na-ci"
 }
 
 variable "resource_group_location" {
