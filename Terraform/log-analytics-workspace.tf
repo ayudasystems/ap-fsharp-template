11a4e8ca363ca3058d09bdfe5a72b73e20d69cb5
SergioHerrero <sergio.herrero@broadsign.com>
2022-08-25 11:30:01 +0200

    Correct LAW resource naming

--- Terraform/log-analytics-workspace.tf
+++ Terraform/log-analytics-workspace.tf
@@ -2,7 +2,7 @@
 
 # Create Log Analytics Workspace related to App insights
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "law-${var.service_name}${var.environment_suffix}-law"
+  name                = "law-${var.service_name}${var.environment_suffix}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   retention_in_days   = 30
