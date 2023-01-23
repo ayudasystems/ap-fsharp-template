7f7487951ed127b1fbaa1277051aa09ae3aae2c0
vlieus <victor.lieu@broadsign.com>
2023-01-23 14:58:56 -0700

    [STAR-416] Setting account subscription and removing commented code

--- azuresetup.sh
+++ azuresetup.sh
@@ -45,15 +45,24 @@ createInfrastructure() {
   
   for environment in "${environments[@]}";
     do
+    
+    if [ $environment == "cloud" ];
+      then
+          # Cloud config
+          az account set --subscription "Ayuda Cloud"
+      else
+        # Labs and Preview config
+        az account set --subscription "Ayuda Preview"
+    fi
+
       for region in "${regions[@]}";
         do
           location=$(parseLocation $region)
-          # echo "LOCATION - $location"
-          RESOURCE_GROUP="$RESOURCE_GROUP_NAME_PREFIX-$inputServiceName-$environment-$region"
+          RESOURCE_GROUP="$RESOURCE_GROUP_NAME_PREFIX-$inputServiceName-$environment-$region-01"
           # az group create -l $location -n "$RESOURCE_GROUP"
           echo "$RESOURCE_GROUP Created." 
           PA_ACCOUNT="$PRINCIPAL_ACCOUNT_PREFIX-$inputServiceName-$environment-$region"
-          # az ad sp create-for-rbac --name "$PA_ACCOUNT_LABS"
+          # az ad sp create-for-rbac --name "$PA_ACCOUNT"
           echo "$PA_ACCOUNT Created."
         done;
     done;
@@ -83,7 +92,7 @@ done
 while [ -z $AZ_REGION ]
 do
   validOptions=("na" "eu" "ap" "all")
-  echo "Environment: (<na>,<eu>,<ap>,<all>). E.g. as-$AZ_ROOT_NAME-$AZ_ENVIRONMENT_NAME-eu"
+  echo "Region: (<na>,<eu>,<ap>,<all>). E.g. as-$AZ_ROOT_NAME-$AZ_ENVIRONMENT_NAME-eu"
   read AZ_REGION
 
   if ! isValidOption "${validOptions[@]}" $AZ_REGION; then
@@ -94,53 +103,10 @@ done
 
 echo "Summary - Creating Resources for as-$AZ_ROOT_NAME-$AZ_ENVIRONMENT_NAME-$AZ_REGION"
 
+# Login with your personal account
+az login
+
 createInfrastructure $AZ_ROOT_NAME $AZ_ENVIRONMENT_NAME $AZ_REGION
-
-
-# # Login with your personal account
-# az login
-
-# # Labs and Preview config
-# az account set --subscription "Ayuda Preview"
-
-# # Config variables
-# RESOURCE_GROUP_NAME_PREFIX="rg-"
-# RESOURCE_GROUP_NAME_SUFFIX_LABS="-labs-na-01"
-# RESOURCE_GROUP_NAME_LOCATION_LABS="northcentralus"
-# RESOURCE_GROUP_NAME_SUFFIX_PREVIEW="-preview-eu-01"
-# RESOURCE_GROUP_NAME_LOCATION_PREVIEW="northeurope"
-# PRINCIPAL_ACCOUNT_PREFIX="PA-"
-# PRINCIPAL_ACCOUNT_SUFFIX_LABS="-labs"
-# PRINCIPAL_ACCOUNT_SUFFIX_PREVIEW="-preview"
-# PRINCIPAL_ACCOUNT_SUFFIX_CLOUD="-cloud"
-
-# # Create labs infra
-# RESOURCE_GROUP_LABS="$RESOURCE_GROUP_NAME_PREFIX$AZ_ROOT_NAME$RESOURCE_GROUP_NAME_SUFFIX_LABS"
-# az group create -l "$RESOURCE_GROUP_NAME_LOCATION_LABS" -n "$RESOURCE_GROUP_LABS" > /dev/null 2>&1
-# echo "$RESOURCE_GROUP_LABS Created."
-
-# PA_ACCOUNT_LABS="$PRINCIPAL_ACCOUNT_PREFIX$AZ_ROOT_NAME$PRINCIPAL_ACCOUNT_SUFFIX_LABS"
-# az ad sp create-for-rbac --name "$PA_ACCOUNT_LABS"
-# echo "$PA_ACCOUNT_LABS Created."
-
-# # Create preview infra
-# RESOURCE_GROUP_PREVIEW="$RESOURCE_GROUP_NAME_PREFIX$AZ_ROOT_NAME$RESOURCE_GROUP_NAME_SUFFIX_PREVIEW"
-# az group create -l "$RESOURCE_GROUP_NAME_LOCATION_PREVIEW" -n "$RESOURCE_GROUP_PREVIEW" > /dev/null 2>&1
-# echo "$RESOURCE_GROUP_PREVIEW Created."
-
-# PA_ACCOUNT_PREVIEW="$PRINCIPAL_ACCOUNT_PREFIX$AZ_ROOT_NAME$PRINCIPAL_ACCOUNT_SUFFIX_PREVIEW"
-# az ad sp create-for-rbac --name "$PA_ACCOUNT_PREVIEW"
-# echo "$PA_ACCOUNT_PREVIEW Created."
-
-# # Cloud config
-# az account set --subscription "Ayuda Cloud"  > /dev/null 2>&1
-
-# ## Create cloud infra
-# # Engineers does not have permissions to create Resource Groups in Cloud
-
-# PA_ACCOUNT_CLOUD="$PRINCIPAL_ACCOUNT_PREFIX$AZ_ROOT_NAME$PRINCIPAL_ACCOUNT_SUFFIX_CLOUD"
-# az ad sp create-for-rbac --name "$PA_ACCOUNT_CLOUD"
-# echo "$PA_ACCOUNT_CLOUD Created."
 
 # echo "################################"
 # echo "Summary to report to Azure Admin"
